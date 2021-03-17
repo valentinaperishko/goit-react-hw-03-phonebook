968f8b35477e2d58dbe1c342b21f74f4f1227461
Valentina Perishko <valentina.perishko@gmail.com>
2021-03-17 16:42:41 -0600

    adds changes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const contact = {
       id: uuid(),
